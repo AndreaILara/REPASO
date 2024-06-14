1ff86cb14edd465f6b35bec0f5ee633a70341779
AndreaILara <andreainigolara@hotmail.com>
2024-06-14 05:46:39 +0200

    Initial commit

---
+++ rev_print.c
@@ -0,0 +1,28 @@
+char *ft_rev_print(char *str)
+
+
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+  int i;
+  i = 0;
+  while(str[i] != '\0')
+  {
+    i++;
+  }
+  return i;
+}
+
+char *ft_rev_print(char *str)
+{
+  int len;
+  len = ft_strlen(str)
+  while(len >= 0)
+  {
+    write(1, &str[len],1);
+    len --;
+  }
+  write(1, "\n", 1);
+  return str;
+}
